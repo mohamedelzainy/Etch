a5419648daba20a428ebdbe43e42db869bb2e368
Mohamed Elzainy <jack200180@gmail.com>
2025-06-14 21:59:57 +0300

    create app and install dependencies

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Button, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Logo</Text>
+      <Button title="Continue With TikTok" onPress={() => {}} />
+    </View>
+  );
+}
